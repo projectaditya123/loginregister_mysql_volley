4037083055d10061e93173060c722399c1cf8b64
miftakhuljannan <miftakhuljannan04@gmail.com>
2023-06-21 13:32:57 +0700

    the end

--- app/src/main/java/com/example/loginregister_mysql_volley/Login.java
+++ app/src/main/java/com/example/loginregister_mysql_volley/Login.java
@@ -6,11 +6,26 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
@@ -35,6 +50,63 @@ public class Login extends AppCompatActivity {
                 startActivity(registerIntent);
             }
         });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sUsername = username.getText().toString();
+                String sPassword = password.getText().toString();
+
+                CheckLogin(sUsername, sPassword);
+            }
+        });
+    }
+    public void CheckLogin(final String username, final String password) {
+        if (checkNetworkConnection()) {
+            progressDialog.show();
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, DbContract.SERVER_LOGIN_URL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonObject = new JSONObject(response);
+                                String resp = jsonObject.getString("server_response");
+                                if (resp.equals("[{\"status\":\"OK\"}]")) {
+                                    Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_SHORT).show();
+                                    Intent dashboardIntent = new Intent(Login.this, Dashboard.class);
+                                    startActivity(dashboardIntent);
+                                } else {
+                                    Toast.makeText(getApplicationContext(), resp, Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                             e.printStackTrace();
+                            }
+
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                   Map<String,String> params = new HashMap<>();
+                    params.put("username", username);
+                    params.put("password", password);
+                    return params;
+                }
+            };
+            VolleyConnection.getInstance(Login.this).addToRequestQue(stringRequest);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    progressDialog.cancel();
+                }
+            }, 2000);
+        } else {
+            Toast.makeText(getApplicationContext(), "Tidak ada koneksi internet", Toast.LENGTH_SHORT).show();
+        }
     }
     public boolean checkNetworkConnection() {
         ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
