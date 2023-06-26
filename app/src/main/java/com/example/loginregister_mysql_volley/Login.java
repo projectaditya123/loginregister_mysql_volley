354b5966bc25a9ed29777dc433bf6f10c05f41df
miftakhuljannan <miftakhuljannan04@gmail.com>
2023-06-26 13:10:25 +0700

    new push again

--- app/src/main/java/com/example/loginregister_mysql_volley/Login.java
+++ app/src/main/java/com/example/loginregister_mysql_volley/Login.java
@@ -74,6 +74,7 @@ public class Login extends AppCompatActivity {
                                     Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_SHORT).show();
                                     Intent dashboardIntent = new Intent(Login.this, Dashboard.class);
                                     startActivity(dashboardIntent);
+                                    finish(); // Hapus aktivitas Login dari tumpukan aktivitas
                                 } else {
                                     Toast.makeText(getApplicationContext(), resp, Toast.LENGTH_SHORT).show();
                                 }
